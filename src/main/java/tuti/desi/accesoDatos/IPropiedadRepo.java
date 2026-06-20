2b27d4ed81d1754ce158fee0db8c38e8c7c773ef
chrisglz14 <chrisgd10s@gmail.com>
2026-06-20 12:19:57 -0300

    IPropiedad

--- src/main/java/tuti/desi/accesoDatos/IPropiedadRepo.java
+++ src/main/java/tuti/desi/accesoDatos/IPropiedadRepo.java
@@ -1,6 +1,9 @@
 package tuti.desi.accesoDatos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import tuti.desi.entidades.Propiedad;
@@ -9,5 +12,10 @@ import tuti.desi.entidades.Propiedad;
 // No hace falta escribir el SQL: Spring Data lo genera solo.
 @Repository
 public interface IPropiedadRepo extends JpaRepository<Propiedad, Long> {
+
+    List<Propiedad> findByDireccion(String direccion);
+    
+    @Query("SELECT p FROM Propiedad p WHERE p.direccion = :direccion AND p.id <> :idDistintoDe")
+    List<Propiedad> findByDireccionAndIdNot(String direccion, Long idDistintoDe);
 
 }

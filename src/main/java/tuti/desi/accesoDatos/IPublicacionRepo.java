af227a3410319500ec08bd3b842073d61ed4046d
candefra <candefra@gmail.com>
2026-06-24 14:08:37 -0300

    epic 3 lista , falta acomodar el html

--- src/main/java/tuti/desi/accesoDatos/IPublicacionRepo.java
+++
@@ -1,9 +0,0 @@
-package tuti.desi.accesoDatos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tuti.desi.entidades.Publicacion;
-
-@Repository
-public interface IPublicacionRepo extends JpaRepository<Publicacion, Long> {
-}
